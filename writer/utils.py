8c5e55ba92ee488b69fd60a3aa3d79f54d72ad28
Diego Dorta <diego.d@variscite.com>
2022-08-30 20:59:11 -0300

    Add utils file
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

---
+++ writer/utils.py
@@ -0,0 +1,118 @@
+# Copyright 2022 Variscite LTD
+# SPDX-License-Identifier: BSD-3-Clause
+
+'''
+Utils function to help on the Variscite SD card writer tool.
+'''
+
+import gzip
+import io
+import math
+import os
+import sys
+from typing import List, Tuple
+
+try:
+    import yaml
+except ImportError:
+    sys.exit("Unable to import package 'yaml'. Please read the documentation")
+
+from .config import CACHEDIR
+from .config import MX8_YAML_CHANGELOG_FILES_B2QT
+from .config import MX8_YAML_CHANGELOG_FILES_DEBIAN
+from .config import MX8_YAML_CHANGELOG_FILES_YOCTO
+from .config import SIZE_NAME
+from .config import SOFTWARE, OS_B2QT, OS_DEBIAN
+from .config import VAR_MODULES
+
+from .ftp import connect_ftp
+from .ftp import retrieve_remote_file
+
+
+def _remote_changelog_path_b2qt(module) -> str:
+    """
+    Returns the remote FTP path for the B2Qt releases.
+    """
+    return os.path.join(module, SOFTWARE,
+                        OS_B2QT, MX8_YAML_CHANGELOG_FILES_B2QT[module])
+
+def _remote_changelog_path_debian(module) -> str:
+    """
+    Returns the remote FTP path for the Debian releases.
+    """
+    return os.path.join(module, SOFTWARE,
+                        OS_DEBIAN, MX8_YAML_CHANGELOG_FILES_DEBIAN[module])
+
+def _remote_changelog_path_yocto(module) -> str:
+    """
+    Returns the remote FTP path for the Yocto releases.
+    """
+    return os.path.join(module, SOFTWARE,
+                        MX8_YAML_CHANGELOG_FILES_YOCTO[module])
+
+def get_readable_size(size_bytes) -> str:
+    """
+    Returns the file size in a readable format.
+    """
+    if size_bytes == 0:
+        return "0B"
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p, 2)
+    size = f"{s}{SIZE_NAME[i]}"
+    return size
+
+def get_images_list_from_ftp(module_name) -> Tuple:
+    """
+    Returns a list of the available images from the FTP.
+    """
+    images = []
+    module = VAR_MODULES[module_name]
+    path = _remote_changelog_path_yocto(module)
+    if not (ftp_r := connect_ftp()):
+        sys.exit("Could not establish FTP connection using the credentials.")
+    file_path, file_name = os.path.split(path)
+    local_file = os.path.join(CACHEDIR, file_name)
+    if os.path.exists(local_file):
+        os.remove(local_file)
+    with open(local_file, 'wb') as f:
+        retrieved = retrieve_remote_file(ftp_r[0], file_name, file_path, f.write)
+    if retrieved:
+        yaml_file_read = read_yaml_file(local_file)
+        for release in yaml_file_read:
+            image = []
+            image_path = release['Release']['Upload Path']
+            file_path, file_name = os.path.split(image_path)
+            file_size = release['Release']['Image Size']
+            image.append(file_name)
+            image.append(file_size)
+            images.append(image)
+        return images, file_path
+    else:
+        return (False, False)
+
+def read_yaml_file(file_path) -> List:
+    """
+    Returns the YAML file content.
+    """
+    with open(file_path, "r") as fp:
+        return list(yaml.safe_load_all(fp))
+
+def get_gzipped_file_size(file_path) -> int:
+    """
+    Returns the file size.
+    """
+    with gzip.open(file_path, 'rb') as f:
+        file_size = f.seek(0, io.SEEK_END)
+    return file_size
+
+def is_gzipped(file_path) -> bool:
+    """
+    Returns if file is gzipped.
+    """
+    with gzip.open(file_path, 'rb') as f:
+        try:
+            f.read(1)
+            return True
+        except gzip.BadGzipFile:
+            return False
