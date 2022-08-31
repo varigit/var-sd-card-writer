653b9d8eb6b0724e96cddbe339721c311c2a7275
Diego Dorta <diego.d@variscite.com>
2022-08-30 23:29:40 -0300

    Add version file
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

--- setup.py
+++ setup.py
@@ -3,14 +3,31 @@
 
 import os
 from setuptools import setup, find_packages
+import re
+import sys
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+PKG = "writer"
+VERSION_FILE = os.path.join(PKG, "_version.py")
+version = "unknown"
+try:
+    verstrline = open(VERSION_FILE, "rt").read()
+except EnvironmentError:
+    pass
+else:
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    mo = re.search(VSRE, verstrline, re.M)
+    if mo:
+        version = mo.group(1)
+    else:
+        sys.exit(f"Unable to find version in {VERSION_FILE}")
+
 setup(
     name = "varwriter",
-    version = "0.0.1",
+    version = version,
     author = "Alifer Moraes, Diego Dorta",
     description = "Variscite SD Card Writer Tool",
     long_description=long_description,
