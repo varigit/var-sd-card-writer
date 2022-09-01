ed805d754228ab3a202587c90c588477d5eb7dd4
Diego Dorta <diego.d@variscite.com>
2022-08-31 23:59:42 -0300

    setup: add new setup file configuration
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

--- setup.py
+++ setup.py
@@ -6,6 +6,47 @@ from setuptools import setup, find_packages
 import re
 import sys
 
+VAR_EXTERNAL_WRITER = os.path.join(os.getcwd(), "writer", "writer.py")
+WRITER_USR = os.path.join(os.environ['HOME'], ".local", "bin", "external_writer_var")
+
+if os.path.exists(WRITER_USR):
+    os.system("rm -rf {}".format(WRITER_USR))
+
+os.system("cp {} {}".format(VAR_EXTERNAL_WRITER, WRITER_USR))
+
+VAR_SD_CARD_WRITER_FOLDER = "varwriter"
+CACHEDIR = os.path.join(os.environ['HOME'], ".cache", VAR_SD_CARD_WRITER_FOLDER)
+
+VAR_SD_CARD_WRITER_ASSETS = "assets"
+CACHEDIR_ASSETS = os.path.join(CACHEDIR, VAR_SD_CARD_WRITER_ASSETS)
+
+ASSETS_CSS_NAME = "writer.css"
+ASSETS_LOGO_NAME = "variscite.png"
+ASSETS_ICON_NAME = "variscite_icon.png"
+
+VAR_WRITER_ASSETS_CSS = os.path.join(os.getcwd(), "writer", "assets", ASSETS_CSS_NAME)
+VAR_WRITER_ASSETS_LOGO = os.path.join(os.getcwd(), "writer", "assets", ASSETS_LOGO_NAME)
+VAR_WRITER_ASSETS_ICON = os.path.join(os.getcwd(), "writer", "assets", ASSETS_ICON_NAME)
+
+#if os.path.exists(CACHEDIR_ASSETS):
+#    os.system("rm -rf {}".format(CACHEDIR_ASSETS))
+
+try:
+    os.mkdir(CACHEDIR)
+except:
+    pass
+
+try:
+    os.mkdir(CACHEDIR_ASSETS)
+except:
+    pass
+
+os.system(f"cp -a {VAR_WRITER_ASSETS_CSS} {CACHEDIR_ASSETS}/{ASSETS_CSS_NAME}")
+os.system(f"cp {VAR_WRITER_ASSETS_LOGO} {CACHEDIR_ASSETS}/{ASSETS_LOGO_NAME}")
+os.system(f"cp {VAR_WRITER_ASSETS_ICON} {CACHEDIR_ASSETS}/{ASSETS_ICON_NAME}")
+
+
+# README FILE
 this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
@@ -26,7 +67,7 @@ else:
         sys.exit(f"Unable to find version in {VERSION_FILE}")
 
 setup(
-    name = "varwriter",
+    name = "var-sd-card-writer",
     version = version,
     author = "Alifer Moraes, Diego Dorta",
     description = "Variscite SD Card Writer Tool",
@@ -36,7 +77,7 @@ setup(
     url = "https://github.com/varigit/var-sd-card-writer",
     packages=find_packages(),
     entry_points = {
-        'console_scripts' : ['varwriter = writer.varwriter:main']
+        'console_scripts' : ['var-sd-card-writer = writer.varwriter:main']
     },
     classifiers=[
         'Development Status :: 3 - Alpha',
